1187bbf95029b58e8839403520c28f8a86d0e7fe
ruthesh0606 <51218518+ruthesh0606@users.noreply.github.com>
2019-06-20 18:06:58 +0530

    Update database.py

--- weather/database.py
+++ weather/database.py
@@ -4,30 +4,12 @@ import pymysql as MySQLdb
 #import MySQLdb
 #from credentails import DB_HOST,DB_USER_NAME,DB_DATABASE_NAME,DB_PASSWORD
 
-class DatabaseOperation:
-	"""
-	It's used to all kind of database operations.
-	"""
+		
 
-	def __init__(self):
-		self.conn = MySQLdb.connect(host="localhost",
-							user="admin",
-							passwd="admin",
-							db="krishi")
-		self.cursor = self.conn.cursor()
-		self.key_order = ['summary','precipIntensity','precipIntensityError','precipProbability','precipType','humidity',
-			'temperature','apparentTemperature','windSpeed','windGust','windBearing','cloudCover','visibility']
-
-	def insert_into_weather_data(self,weather_data):
-		raw_query = """INSERT INTO weatherlog 
-						(summary,precipIntensity,precipProbability,
-						precipType,humidity,temperature,apparentTemperature,windSpeed,
-						windGust,windBearing,cloudCover,visibility)
-						values 
-						(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-		self.cursor.execute(raw_query,weather_data)
-		self.conn.commit()
-		print ('Weather data inserted in database.')
-		#except:
-		#	self.conn.rollback()
-		#	print ('Database insertion failed.')
+def insert_into_weather_data(weather_data):
+	conn = MySQLdb.connect(host="localhost",user="admin",passwd="admin",db="krishi")
+	cursor = self.conn.cursor()
+	key_order = ['summary','precipIntensity','precipIntensityError','precipProbability','precipType','humidity','temperature','apparentTemperature','windSpeed','windGust','windBearing','cloudCover','visibility']
+	raw_query = """INSERT INTO weatherlog(summary,precipIntensity,precipProbability,precipType,humidity,temperature,apparentTemperature,windSpeed,windGust,windBearing,cloudCover,visibility)values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+	cursor.execute(raw_query,weather_data)
+	conn.commit()
