f9ca794903d70f666abd964e057fc272c2bfad89
ruthesh0606 <51218518+ruthesh0606@users.noreply.github.com>
2019-06-20 18:11:52 +0530

    Update database.py

--- weather/database.py
+++ weather/database.py
@@ -10,7 +10,7 @@ def insert_into_weather_data(weather_data):
 	conn = MySQLdb.connect(host="localhost",user="admin",passwd="admin",db="krishi")
 	cursor = conn.cursor()
 	key_order = ['summary','precipIntensity','precipIntensityError','precipProbability','precipType','humidity','temperature','apparentTemperature','windSpeed','windGust','windBearing','cloudCover','visibility']
-	raw_query = """INSERT INTO weatherlog(summary,precipIntensity,precipProbability,precipType,humidity,temperature,apparentTemperature,windSpeed,windGust,windBearing,cloudCover,visibility)values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+	raw_query = """INSERT INTO weatherlog(summary,precipIntensity,precipProbability,precipType,humidity,temperature,apparentTemperature,windSpeed,windGust,windBearing,cloudCover,visibility)values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
 	cursor.execute(raw_query,weather_data)
 	conn.commit()
 	conn.close()
